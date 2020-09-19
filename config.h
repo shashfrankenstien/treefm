7bdbd8191a265dfed7b6486558263bd41eaefa76
Shashank Gopikrishna <shashank.gopikrishna@gmail.com>
2020-09-19 13:58:34 -0400

    change config variables to preproc defs

--- config.h
+++ config.h
@@ -1,9 +1,9 @@
 
 
 //config defaults
-static int main_border_r = 1;
-static int main_border_c = 1;
-static int intern_pad_r = 0;
-static int intern_pad_c = 1;
+#define main_border_r 1
+#define main_border_c 1
+#define intern_pad_r 0
+#define intern_pad_c 1
 
-static bool main_light_borders = true;
+#define main_light_borders true
